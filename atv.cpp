620494b981ba200634d310af070980fb79af4ddc
Samir <emaildesamir@gmail.com>
2019-10-01 12:32:34 -0300

    Questões 6-8
    DONE

--- atv.cpp
+++ atv.cpp
@@ -1,8 +1,24 @@
+
+#include <array>
+#include <string>
+#include <locale>
+#include <ios>
 #include <iostream>
-#include <vector>
-#include <iterator>
+
+class my_fr : public std::numpunct< char > {
+protected:
+    char do_decimal_point() const { return ','; }
+    char do_thousands_sep() const { return '.'; }
+    std::string do_grouping() const { return "\3"; }
+    std::string do_truename() const { return "True";  }
+    std::string do_falsename() const { return "False"; }
+};
+
 
 #include "include/atv.h"
+#include "graal.h"
+
+
 const bool cmp(const int &a, const int &b)
 {
     return a < b; // If a < b return true else return false;
@@ -13,7 +29,7 @@ const bool predicate(const int &a)
     return a > 30;   
 }
 
-const bool equal(const int &a, const int &b)
+const bool test_equal(const int &a, const int &b)
 { 
     return a == b;   
 }
@@ -135,13 +151,86 @@ void q5()
         std::cout << e << " ";
     }
     
-    auto result = lp1::find( std::begin(A), std::end(A), 30, equal );
+    auto result = lp1::find( std::begin(A), std::end(A), 30, test_equal );
 
+    std:: cout << std::endl  << "Result: " << *result << std::endl;
+}
+
+
+
+void q6()
+{
+    int A[50];    
+    int count = 0;
+
+    std::cout << "A: " << std::endl; 
+    for(int & e : A) //Range for
+    {
+        e = count;
+        count += 1;
+        std::cout << e << " ";
+    }
+    
+    auto result1 = lp1::all_of( std::begin(A), std::end(A), predicate );
+    auto result2 = lp1::any_of( std::begin(A), std::end(A), predicate );
+    auto result3 = lp1::none_of( std::begin(A), std::end(A), predicate );
+
+    std:: cout << std::endl;
+    std:: cout << std::endl  << "Result 1: " << result1 << std::endl;
+    std:: cout <<               "Result 2: " << result2 << std::endl;
+    std:: cout <<               "Result 3: " << result3 << std::endl;
+}
+
+void q7()
+{
+    int A[50];
+    int B[50];
+    int C[60];       
+    int count = 0;
+
+    std::cout << "A: " << std::endl; 
+    for(int & e : A) //Range for
+    {
+        e = count;
+        count += 1;
+        std::cout << e << " ";
+    }
+    count = 0;
+    std::cout << std::endl << "B: " << std::endl; 
+    for(int & e : B) //Range for
+    {
+        e = count;
+        count += 2;
+        std::cout << e << " ";
+    }
+    count = 0;
+    std::cout << std::endl << "C: " << std::endl; 
+    for(int & e : C) //Range for
+    {
+        e = count;
+        count += 1;
+        std::cout << e << " ";
+    }
+
+    auto result1 = lp1::equal( std::begin(A), std::end(A), std::begin(B), test_equal );
+    std:: cout << std::endl  << "Result 1: " << result1 << std::endl;
+    auto result2 = lp1::equal( std::begin(A), std::end(A), std::begin(C), std::end(C), test_equal );
+    std:: cout << std::endl  << "Result 2: " << result2 << std::endl;
+}
+
+void q8()
+{
+    int A[11] = { 1,1,2,2,3,3,4,5,4,7,1};
+
+    auto result = lp1::unique(std::begin(A), std::end(A), test_equal);
     std:: cout << std::endl  << "Result: " << *result << std::endl;
 }
 
 int main()
 {
+    std::cout.imbue(std::locale(std::locale(), new my_fr));
+    std::cout << std::boolalpha;
+    /*
     std::cout << std::endl;
     std::cout << " >>> QUESTION NUMBER 1 <<<" << std::endl;
     q1();
@@ -157,5 +246,18 @@ int main()
     std::cout << std::endl;
     std::cout << ">>> QUESTION NUMBER 5 <<<" << std::endl;
     q5();
+    std::cout << std::endl;
+    std::cout << ">>> QUESTION NUMBER 6 <<<" << std::endl;
+    q6();
+    std::cout << std::endl;
+    std::cout << ">>> QUESTION NUMBER 7 <<<" << std::endl;
+    q7();
+    */
+    std::cout << std::endl;
+    std::cout << ">>> QUESTION NUMBER 8 <<<" << std::endl;
+    q8();
+
+    
+    //std::cout << "Waiting to be Implemented" << std::endl;
     return 0;    
 }
--- include/atv.h
+++ include/atv.h
@@ -54,7 +54,7 @@ namespace lp1
         return d_first;
     }
 
-    template < typename Itr, typename Predicate >    
+    template < typename Itr, typename Predicate >
     Itr find_if( Itr first, Itr last, Predicate p)
     {
         while(first != last)
@@ -70,7 +70,7 @@ namespace lp1
         return first;
     }
 
-    template < typename Itr, typename T, typename Equal >    
+    template < typename Itr, typename T, typename Equal >
     Itr find( Itr first, Itr last, const T & value, Equal eq )
     {
         while(first != last)
@@ -86,6 +86,146 @@ namespace lp1
         return first;
     }
 
+    template < typename Itr, typename Predicate >
+    bool all_of( Itr first, Itr last, Predicate p )
+    {
+        if(first == last){return true;}
+
+        while(first != last)
+        {
+            if (!p(*first))
+            {
+                return false;
+            } 
+
+            first++;
+        }
+
+        return true;
+    }
+
+    template < typename Itr, typename Predicate >
+    bool any_of( Itr first, Itr last, Predicate p )
+    {
+        if(first == last){return true;}
+
+        while(first != last)
+        {
+            if (!p(*first))
+            {
+                return false;
+            } 
+
+            first++;
+        }
+        
+        return true;
+    }
+
+    template < typename Itr, typename Predicate >
+    bool none_of( Itr first, Itr last, Predicate p )
+    {
+        if(first == last){return true;}
+
+        while(first != last)
+        {
+            if (p(*first))
+            {
+                return false;
+            } 
+
+            first++;
+        }
+
+        return true;
+    }
+
+    template <typename Itr, typename Equal>
+    bool equal(Itr first1, Itr last1, Itr first2, Equal eq)
+    {
+        while(first1 < last1)
+        {
+            if(!eq(*first1, *first2))
+            {
+                return false;                
+            }
+
+            first1++;
+            first2++;
+        }
+
+        return true;
+    }
+
+    template <typename Itr, typename Equal>
+    bool equal (Itr first1, Itr last1, Itr first2, Itr last2, Equal eq)
+    {
+        if(last1-first1 != last2-first2)
+        {
+            return false;
+        }
+
+        while(first1 != last1)
+        { 
+            if(!eq(*first1, *first2))
+            {
+                return false;
+            }                
+            
+            first1++;
+            first2++;
+        }
+
+        return true;
+    }
+
+    template<typename Itr, typename Equal>
+    Itr unique(Itr first1, Itr last1, Equal eq)
+    {        
+        Itr inicio = first1;
+        Itr auxiliar = first1 + 1;
+
+        int size1 = (last1 - 1) - inicio;
+        int array2[size1];
+
+        Itr ponteiro = first1; 
+        Itr first2 = &array2[0];
+        Itr inicio2 = &array2[0];
+        Itr last2 = &array2[size1];
+
+        while(inicio < last1)
+        {
+            if (auxiliar != inicio && eq(*auxiliar,*inicio))
+            {
+                *last2 = *inicio;
+                last2--;
+                inicio++;
+                auxiliar = first1;
+            }
+            else if(auxiliar == last1 && !eq(*auxiliar,*inicio))
+            {
+                *first2 = *inicio;
+                first2++;
+                inicio++;
+                auxiliar = first1;
+                ponteiro++;                
+            }
+            else
+            {
+                auxiliar++;
+            }
+        }
+        
+        for (int i = 0; i <= size1; i++)
+        {
+            *first1 = *inicio2;
+            std::cout << *first1 << " ";
+            first1++;
+            inicio2++;
+        }
+
+        return ponteiro;
+    }
 }
 
 #endif
